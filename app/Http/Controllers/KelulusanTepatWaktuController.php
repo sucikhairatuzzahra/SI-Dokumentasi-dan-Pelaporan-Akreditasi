db7192a664d3eb98229d2bed4c9f271ecedfed37
alvnfaiz <alvin.faiz.10@gmail.com>
2023-11-10 17:35:37 +0700

    Change controller

--- app/Http/Controllers/KelulusanTepatWaktuController.php
+++ app/Http/Controllers/KelulusanTepatWaktuController.php
@@ -4,11 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Exports\KelulusanTepatWaktuExport;
 use App\Models\KelulusanTepatWaktu;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Collection;
-use Maatwebsite\Excel\Facades\Excel;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class KelulusanTepatWaktuController extends Controller
 {
@@ -24,16 +22,58 @@ class KelulusanTepatWaktuController extends Controller
         // dd($data);
         return view('admin.page.kelulusan_tepat_waktu.index', compact('data'));
     }
+
     public function admprodiIndex(Request $request)
     {
         $data = KelulusanTepatWaktu::all();
 
-        $akhir_ts = KelulusanTepatWaktu::where('tahun_lulus', Carbon::now()->format('Y'))
-        ->sum('jml_lulusan');
-        return view('admprodi.page.kelulusan_tepat_waktu.index', compact('data','akhir_ts'));
+        foreach ($data as $i => $data1) {
+            // data akhir_ts = data mahasiswa yang lulus di tahun ini, dan tahun masuk di $data['tahun_masuk]
+            $akhir_ts = KelulusanTepatWaktu::where('id_pt_unit', $data1['id_pt_unit'])
+            ->where('tahun_masuk', $data1['tahun_masuk'])
+            ->where('tahun_lulus', Carbon::now()->format('Y'))
+            ->sum('jml_lulusan');
+            $data[$i]['akhir_ts'] = $akhir_ts;
+            // data akhir_ts_1 = data mahasiswa yang lulus di 1 tahun yang lalu, dan tahun masuk di $data['tahun_masuk]
+            $akhir_ts_1 = KelulusanTepatWaktu::where('id_pt_unit', $data1['id_pt_unit'])
+            ->where('tahun_masuk', $data1['tahun_masuk'])
+            ->where('tahun_lulus', Carbon::now()->subYear()->format('Y'))
+            ->sum('jml_lulusan');
+            $data[$i]['akhir_ts_1'] = $akhir_ts_1;
+            // $akhir_ts_2
+            $akhir_ts_2 = KelulusanTepatWaktu::where('id_pt_unit', $data1['id_pt_unit'])
+            ->where('tahun_masuk', $data1['tahun_masuk'])
+            ->where('tahun_lulus', Carbon::now()->subYear(2)->format('Y'))
+            ->sum('jml_lulusan');
+            $data[$i]['akhir_ts_2'] = $akhir_ts_2;
+            // $akhir_ts_3
+            $akhir_ts_3 = KelulusanTepatWaktu::where('id_pt_unit', $data1['id_pt_unit'])
+            ->where('tahun_masuk', $data1['tahun_masuk'])
+            ->where('tahun_lulus', Carbon::now()->subYear(3)->format('Y'))
+            ->sum('jml_lulusan');
+            $data[$i]['akhir_ts_3'] = $akhir_ts_3;
+            // $akhir_ts_4
+            $akhir_ts_4 = KelulusanTepatWaktu::where('id_pt_unit', $data1['id_pt_unit'])
+            ->where('tahun_masuk', $data1['tahun_masuk'])
+            ->where('tahun_lulus', Carbon::now()->subYear(4)->format('Y'))
+            ->sum('jml_lulusan');
+            $data[$i]['akhir_ts_4'] = $akhir_ts_4;
+            // $akhir_ts_5
+            $akhir_ts_5 = KelulusanTepatWaktu::where('id_pt_unit', $data1['id_pt_unit'])
+            ->where('tahun_masuk', $data1['tahun_masuk'])
+            ->where('tahun_lulus', Carbon::now()->subYear(5)->format('Y'))
+            ->sum('jml_lulusan');
+            $data[$i]['akhir_ts_5'] = $akhir_ts_5;
+            // $akhir_ts_6
+            $akhir_ts_6 = KelulusanTepatWaktu::where('id_pt_unit', $data1['id_pt_unit'])
+            ->where('tahun_masuk', $data1['tahun_masuk'])
+            ->where('tahun_lulus', Carbon::now()->subYear(6)->format('Y'))
+            ->sum('jml_lulusan');
+            $data[$i]['akhir_ts_6'] = $akhir_ts_6;
+        }
 
+        return view('admprodi.page.kelulusan_tepat_waktu.index', compact('data'));
 
-    
         // $tahun_masuk = $request->get('tahun_masuk');
         // $id_pt_unit = $request->get('id_pt_unit');
 
@@ -69,14 +109,15 @@ class KelulusanTepatWaktuController extends Controller
         // return view('admprodi.page.kelulusan_tepat_waktu.index', [
         //     'data' => $data_lulusan,
         // ]);
-        
     }
+
     public function kaprodiIndex()
     {
         $data = KelulusanTepatWaktu::paginate('20');
 
         return view('kaprodi.page.kelulusan_tepat_waktu.index', compact('data'));
     }
+
     /**
      * Show the form for creating a new resource.
      *
@@ -95,7 +136,6 @@ class KelulusanTepatWaktuController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -110,7 +150,6 @@ class KelulusanTepatWaktuController extends Controller
             'wisuda_ke' => $request->wisuda_ke,
             'masa_studi' => $request->masa_studi,
             'jml_mhs_aktif' => $request->jml_mhs_aktif,
-
         ]);
         if ($input) {
             return redirect('kelulusan_tepatwaktu')->with('pesan', 'Data berhasil disimpan');
@@ -125,31 +164,33 @@ class KelulusanTepatWaktuController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
         $data['editData'] = KelulusanTepatWaktu::find($id);
+
         return view('admprodi.page.kelulusan_tepat_waktu.form_edit', $data);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -178,18 +219,20 @@ class KelulusanTepatWaktuController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
         $kelulusan = KelulusanTepatWaktu::findOrFail($id); // Ganti dengan model dan nama tabel yang sesuai
         $kelulusan->delete();
+
         return redirect()->route('kelulusan_tepatwaktu')->with('success', 'Data Kelulusan Tepat Waktu Berhasil Dihapus');
     }
 
     public function download()
     {
-        return Excel::download(new KelulusanTepatWaktuExport, 'Kelulusan Tepat Waktu.xlsx');
+        return Excel::download(new KelulusanTepatWaktuExport(), 'Kelulusan Tepat Waktu.xlsx');
     }
 }
