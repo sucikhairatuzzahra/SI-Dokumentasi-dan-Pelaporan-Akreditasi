f6c73938e353617649c96b7a8536d4d8d326961f
Andi-IM <andi.irhamm@gmail.com>
2023-12-09 22:46:38 +0700

    fitur upload untuk pendanaan

--- resources/views/pendanaan/edit.blade.php
+++ resources/views/pendanaan/edit.blade.php
@@ -1,91 +1,99 @@
 @extends('layouts.app')
 @section('content')
-    {{-- page header  --}}
-    <div class="page-header">
-        <div class="page-block">
-            <div class="row align-items-center">
-                <div class="col-md-8">
-                    <div class="page-header-title">
-                        <h5 class="m-b-10">Sumber Pendanaan Program Studi</h5>
-                        <p class="m-b-0">Form Sumber Pendanaan Program Studi</p>
-                    </div>
+{{-- page header  --}}
+<div class="page-header">
+    <div class="page-block">
+        <div class="row align-items-center">
+            <div class="col-md-8">
+                <div class="page-header-title">
+                    <h5 class="m-b-10">Sumber Pendanaan Program Studi</h5>
+                    <p class="m-b-0">Form Sumber Pendanaan Program Studi</p>
                 </div>
-                <div class="col-md-4">
-                    <ul class="breadcrumb-title">
-                        <li class="breadcrumb-item">
-                            <a href="index.html"> <i class="fa fa-home"></i> </a>
-                        </li>
+            </div>
+            <div class="col-md-4">
+                <ul class="breadcrumb-title">
+                    <li class="breadcrumb-item">
+                        <a href="index.html"> <i class="fa fa-home"></i> </a>
+                    </li>
 
-                        <li class="breadcrumb-item"><a href="#!">Sumber Pendanaan Program Studi</a>
-                        </li>
-                    </ul>
-                </div>
+                    <li class="breadcrumb-item"><a href="#!">Sumber Pendanaan Program Studi</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
-    <!-- Page-header end -->
-    <div class="pcoded-inner-content">
-        <!-- Main-body start -->
-        <div class="main-body">
-            <div class="page-wrapper">
-                <!-- Page-body start -->
-                <div class="page-body">
-                    <!-- Basic table card start -->
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h5>Form Sumber Pendanaan Program Studi</h5>
-                                    {{-- <span>Add class of <code>.form-control</code> with <code>&lt;input&gt;</code> tag</span> --}}
+</div>
+<!-- Page-header end -->
+<div class="pcoded-inner-content">
+    <!-- Main-body start -->
+    <div class="main-body">
+        <div class="page-wrapper">
+            <!-- Page-body start -->
+            <div class="page-body">
+                <!-- Basic table card start -->
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5>Form Sumber Pendanaan Program Studi</h5>
+                                @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
                                 </div>
-                                <div class="card-block">
-                                    <form action="{{ route('pendanaan.update', $editData->id) }}" method="POST">
-                                        @csrf
-                                        @method('put')
-                                        <div class="form-group">
-                                            <label for="sumber_dana">Sumber Dana</label>
-                                            <input type="text" class="form-control" id="sumber_dana" name="sumber_dana"
-                                                value="{{ $editData->sumber_dana }}">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="jumlah">Jumlah</label>
-                                            <input type="text" class="form-control" id="jumlah" name="jumlah"
-                                                value="{{ $editData->jumlah }}">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="bukti">Bukti</label>
-                                            <input type="file" name="bukti" id="bukti" class="form-control">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="keterangan">Keterangan</label>
-                                            <input type="text" class="form-control" id="keterangan" name="keterangan"
-                                                value="{{ $editData->keterangan }}">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="pt_unit">Unit Kerja</label>
-                                            <input type="hidden" class="form-control" name="id_pt_unit"
-                                                value="{{ $editData->ptUnit->id }}" required>
-                                            <input type="text" class="form-control"
-                                                value="{{ $editData->ptUnit->kode_pt_unit }}" disabled>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary">Simpan</button>
-                                    </form>
-                                    <button class="btn btn-light" onclick="window.history.back()">Cancel</button>
-                                </div>
-
+                                @endif
+                                {{-- <span>Add class of <code>.form-control</code> with <code>&lt;input&gt;</code> tag</span> --}}
                             </div>
+                            <div class="card-block">
+                                <form action="{{ route('pendanaan.update', $editData->id) }}" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    @method('put')
+                                    <div class="form-group">
+                                        <label for="sumber_dana">Sumber Dana</label>
+                                        <input type="text" class="form-control" name="sumber_dana" value="{{ $editData->sumber_dana }}" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="jumlah">Jumlah</label>
+                                        <input type="text" class="form-control" name="jumlah" value="{{ $editData->jumlah }}" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="bukti">Bukti</label>
+                                        <br>
+                                        <strong>bukti saat ini: {{$editData->bukti}}</strong>
+                                        <hr>
+                                        <input type="file" name="bukti" class="form-control">
+                                        <p><strong>Biarkan kosong jika tidak ingin mengganti bukti</strong></p>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="keterangan">Keterangan</label>
+                                        <input type="text" class="form-control" name="keterangan" value="{{ $editData->keterangan }}" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="pt_unit">Unit Kerja</label>
+                                        <input type="hidden" class="form-control" name="id_pt_unit" value="{{ $editData->ptUnit->id }}" required>
+                                        <input type="text" class="form-control" value="{{ $editData->ptUnit->kode_pt_unit }}" disabled>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Simpan</button>
+                                </form>
+                                <button class="btn btn-light" onclick="window.history.back()">Cancel</button>
+                            </div>
+
                         </div>
                     </div>
                 </div>
-                <!-- Basic table card end -->
             </div>
-            <!-- Page-body end -->
+            <!-- Basic table card end -->
         </div>
+        <!-- Page-body end -->
     </div>
-    <!-- Main-body end -->
+</div>
+<!-- Main-body end -->
 
-    <div id="styleSelector">
+<div id="styleSelector">
 
-    </div>
-    </div>
+</div>
+</div>
 @endsection
